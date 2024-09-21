74f04777309a931c19dbf35e8a51328b6d95c693
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-09-21 17:19:13 +0545

    adding navbar and setup flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          600: '#1C64F2',
+          700: '#1A56DB'
+        }
+      }
+    }
+  },
+  plugins: [
+    // require('flowbite/plugin')
   ]
 }
