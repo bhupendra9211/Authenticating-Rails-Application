57af2fc62778979ac7cb31aa86ee9b719c37230d
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-09-26 09:08:24 +0545

    implemented forgot complete email send status show into custome page

--- app/controllers/admin_users/passwords_controller.rb
+++ app/controllers/admin_users/passwords_controller.rb
@@ -7,9 +7,17 @@ class AdminUsers::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      # respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
+      render :forgot_complete, status: :see_other
+    else
+      respond_with(resource)
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
